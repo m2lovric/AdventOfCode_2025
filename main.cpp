4ec03b60ef8b8b81bb379c76815e53a37f5d4729
Matteo Lovrić <matteolovric@Matteos-MacBook-Air.local>
2025-12-01 20:18:26 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <fstream>
+#include <format>
+
+using namespace std;
+
+int main() {
+    int dial[100];
+    for (int i = 0; i < 100; ++i) {
+        dial[i] = i;
+    }
+
+    int pointer = 50;
+    int counter = 0;
+
+    ifstream file("../sequence.txt");
+    string line;
+
+    if (file.is_open()) {
+        while (getline(file, line)) {
+            int num = stoi(line.substr(1, line.length()));
+            if (num > 99) {
+                string str_num = format("{}", num);
+                num = stoi(str_num.substr(1, line.length()));
+            }
+            if (line[0] == 'R') {
+                if (pointer + num > 99) {
+                    pointer = pointer + num - 100;
+                } else {
+                    pointer = pointer + num;
+                }
+            } else {
+                if (pointer - num < 0) {
+                    pointer = pointer - num + 100;
+                } else {
+                    pointer = pointer - num;
+                }
+            }
+
+            //cout << pointer << endl;
+
+            if (pointer == 0) {
+                counter++;
+            }
+        }
+        file.close();
+    } else {
+        cerr << "Unable to open file" << endl;
+    }
+
+    cout << "Rezultat je : " << counter << endl;
+    return 0;
+}
